a21cbdf5356dd2ca4e1aa66367d5be3b5f89a639
LinHong <linlin.hong1@gmail.com>
2024-10-08 17:03:05 -0400

    test

--- src/app/sections/AboutMe.jsx
+++ src/app/sections/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
     //         <p className='font-header5-bold'>Hi! I'm Lin Hong,</p>
     //         <p className='font-paragraph'>A Computer Science student at the University of Waterloo, with internships under my belt at Shopify, Google, and Tesla.<br /> <br /> I'm passionate about leveraging data science and machine learning to solve real-world challenges, having led projects that enhance customer experiences and optimize systems. <br /><br /> My hackathon journey includes over 20 competitions, with notable successes, including a win at the MIT Hackathon for a blockchain project. <br /><br /> When I'm not coding, I enjoy digital art, yoga, and outdoor adventures. <br /><br /> I'm all about innovation and impact, currently seeking Summer 2024 internships. If you're on a mission to make a difference or just want to chat, let's connect! 🚀🧘‍♂️🌟</p>
     //     </div>
-    // </div>
+    // </div>sdafsdaf
     <div className='p-parent w-screen h-screen flex flex-col items-center justify-center z-10'>
         <p className='font-header2-bold'>Hi! I'm Lin Hong 👋</p>
         <div className='p-14 rounded-3xl w-[55vw] h-full text-center'>
